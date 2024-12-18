762ec05ed09466cbbcdedd30aeaa1b9e8a87af37
Avijit bera <avijitbera775@gmail.com>
2024-12-18 08:59:03 +0530

    created database client methods

--- client/database_client.ts
+++ client/database_client.ts
@@ -1,8 +1,29 @@
 import {AuthConfig, ClientConfig, UserStore} from './types'
 import {Connection} from './connection'
+import { AuthClient } from './auth_client'
 
 
 export class DatabaseClient {
     private connection: Connection
+    private authClient: AuthClient;
+
+    constructor(
+        config: ClientConfig = {},
+        userStore: UserStore
+    ){
+        this.connection = new Connection(config)
+        this.authClient = new AuthClient(config, userStore)
+    }
+    async connect(): Promise<void> {
+        await this.connection.connect()
+    }
+
+    async login(auth:AuthConfig): Promise<void> {
+        await this.authClient.login(auth);
+    }
+
+    async register(auth:AuthConfig): Promise<void> {
+        await this.authClient.register(auth);
+    }
     
 }
