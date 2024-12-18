045c1542ed5cfe9072712770952306145e12d556
Avijit bera <avijitbera775@gmail.com>
2024-12-18 09:04:01 +0530

    created database client methods like get set delete clear keys

--- client/database_client.ts
+++ client/database_client.ts
@@ -25,5 +25,60 @@ export class DatabaseClient {
     async register(auth:AuthConfig): Promise<void> {
         await this.authClient.register(auth);
     }
+
+    private async sendAuthenticatedCommand<T>(command: any): Promise<T> {
+        if(!this.authClient.isAuthenticated()){
+            throw new Error('Not authenticated')
+        }
+        const userId = this.authClient.getUser()
+        const response = await this.connection.send<T>({
+            ...command,
+            userId
+        })
+        if(response.status === 'error'){
+            throw new Error(response.message)
+        }
+        return response.data!;
+    }
+
+    async set(key:string, value:any, expiresIn?:number): Promise<string> {
+        return this.sendAuthenticatedCommand({
+            command: 'SET',
+            key,
+            value,
+            expiresIn
+        })
+    }
+
+    async get(key:string): Promise<any> {
+        return this.sendAuthenticatedCommand({
+            command: 'GET',
+            key
+        })
+    }
+
+    async delete(key:string): Promise<string> {
+        return this.sendAuthenticatedCommand({
+            command: 'DELETE',
+            key
+        })
+    }
+
+    async clear(): Promise<string> {
+        return this.sendAuthenticatedCommand({
+            command: 'CLEAR'
+        })
+    }
+
+    async keys(): Promise<string[]> {
+        return this.sendAuthenticatedCommand({
+            command: 'KEYS'
+        })
+    }
+
+    disconnect(): void {
+        this.connection.disconnect()
+        this.authClient.disconnect()
+    }
     
 }
