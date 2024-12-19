fa013e64ce9a250385ad9786e2802264acd37805
Avijit bera <avijitbera775@gmail.com>
2024-12-19 15:30:57 +0530

    create i memoey store file and created getuserStorage method

--- src/database/in_memory_store.ts
+++ src/database/in_memory_store.ts
@@ -1,7 +1,25 @@
-import {DatabaseOperations, DatabaseCommand, DatabaseResponse} from '../types/database'
-
+import {DatabaseOperations, DatabaseCommand, DatabaseResponse, DatabaseEntery} from '../types/database'
+import { ExpirationManager } from "./expiration_manager";
 export class InMemoryStore<T = any> implements DatabaseOperations<T> {
-    private storage: Map<string, Map<string>>
+    private storage: Map<string, Map<string, DatabaseEntery<T>>>;
+    private expirationManager: ExpirationManager;
+
+    constructor(){
+        this.storage = new Map<string, Map<string, DatabaseEntery<T>>>()
+        this.expirationManager = new ExpirationManager()
+        
+        this.expirationManager.on('expired', (key: string) =>{
+
+        })
+    }
+
+    private getUserStorage(accountId: string): Map<string, DatabaseEntery<T>>{
+        const userStorage = this.storage.get(accountId)
+        if(!userStorage){
+            this.storage.set(accountId, new Map<string, DatabaseEntery<T>>())
+        }
+        return this.storage.get(accountId)!;
+    }
 
     get: (key: string, accountId: string) => Promise<T | null>;
     set: (key: string, value: T, accountId: string, expiresIn?: number) => Promise<string>;
