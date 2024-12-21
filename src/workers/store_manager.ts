5c6786f18e4593238631daf3cd97a90009448d6b
Avijit bera <avijitbera775@gmail.com>
2024-12-21 09:46:18 +0530

    created get user storage method in store manager

--- src/workers/store_manager.ts
+++ src/workers/store_manager.ts
@@ -7,4 +7,18 @@ export class StoreManager<T = any> {
     constructor(){
         this.storage = new Map<string, Map<string, DatabaseEntery<T>>>()
     }
+
+    static getInstance(): StoreManager {
+        if (!StoreManager.instance) {
+          StoreManager.instance = new StoreManager();
+        }
+        return StoreManager.instance;
+    }
+    private getUserStorage(accountId: string): Map<string, DatabaseEntery<T>>{
+        const userStorage = this.storage.get(accountId)
+        if(!userStorage){
+            this.storage.set(accountId, new Map<string, DatabaseEntery<T>>())
+        }
+        return this.storage.get(accountId)!;
+    }
 }
