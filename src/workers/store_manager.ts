fcae392f513c85d34416e74bcbf52d07ac1d6f36
Avijit bera <avijitbera775@gmail.com>
2024-12-21 09:49:31 +0530

    created command method in store manager

--- src/workers/store_manager.ts
+++ src/workers/store_manager.ts
@@ -1,4 +1,5 @@
 import {DatabaseEntery} from '../types/database'
+import { logger } from '../utils/logger';
 export class StoreManager<T = any> {
 
     private static instance: StoreManager;
@@ -21,4 +22,64 @@ export class StoreManager<T = any> {
         }
         return this.storage.get(accountId)!;
     }
+
+    async get(key: string, accountId: string):Promise<T | null>{
+        console.log({key, accountId})
+        
+        const accountStorage = this.getUserStorage(accountId || 'default')
+       
+        const entry = accountStorage.get(key)
+       
+        if(!entry){
+            logger.info(`Key ${key} not found in account ${accountId || 'default'}`)
+            return null
+        }
+        if(entry.expiresAt && Date.now() >= entry.expiresAt){
+            accountStorage.delete(key)
+            logger.info(`Key ${key} expired in account ${accountId || 'default'}`)
+            return null
+        }
+        logger.info(`Key ${key} found in account ${accountId || 'default'}`)
+        return entry.value;
+    };
+
+    async set(key: string, value: T, accountId: string, expiresIn?: number): Promise<string>{
+        const accountStorage = this.getUserStorage(accountId || 'default')
+        
+        const entry: DatabaseEntery<T> = { value }
+        if(expiresIn !== undefined && expiresIn > 0){
+            entry.expiresAt = Date.now() + (expiresIn * 1000);
+            // this.expirationManager.scheduleExpiration(
+            //     this.getFullKey(accountId || 'default', key),
+            //     entry.expiresAt
+            // )
+        }
+        accountStorage.set(key, entry)
+       
+        logger.info(`Set key ${key} in account ${accountId || 'default'}`, {value})
+        
+        return 'OK';
+    };
+
+    async delete(key: string, accountId: string): Promise<string>{
+        const accountStorage = this.getUserStorage(accountId || 'default')
+        // this.expirationManager.clearExpirationTimer(this.getFullKey(accountId || 'default', key))
+        return accountStorage.delete(key) ? 'OK' : 'NOT_FOUND';
+    };
+    async clear(accountId: string):Promise<string>{
+        const accountStorage = this.getUserStorage(accountId || 'default')
+        // for (const key of accountStorage.keys()){
+        //     this.expirationManager.clearExpirationTimer(this.getFullKey(accountId || 'default', key))
+        // }
+        accountStorage.clear()
+        return 'OK';
+    };
+
+    async keys(accountId: string):Promise<string[]>{
+        const accountStorage = this.getUserStorage(accountId)
+        const now = Date.now()
+        return Array.from(accountStorage.entries())
+        .filter(([_, entry]) => !entry.expiresAt || entry.expiresAt > now)
+        .map(([key]) => key);
+    };
 }
