edc70026ff5befeafcfc1caf21fef6f3f2911b29
Avijit bera <avijitbera775@gmail.com>
2024-12-28 09:48:34 +0530

    completed build script

--- scripts/build.js
+++ scripts/build.js
@@ -18,4 +18,23 @@ execSync('npm run build', {stdio: 'inherit'})
 console.log('Building executable...')
 execSync('npm run build:exe', {stdio: 'inherit'})
 
+switch (process.platform){
+    case 'linux':
+        console.log('Building Linux packages...')
+        execSync('npm run build:linux', {stdio: 'inherit'})
+        console.timeLog('Creating DEB package...')
+        execSync('npm run build:deb', {stdio: 'inherit'})
+        console.timeLog('Creating RPM package...')
+        execSync('npm run build:rpm', {stdio: 'inherit'})
+        break;
+    case 'win32':
+        console.log('Building Windows packages...')
+        execSync('npm run build:win', {stdio: 'inherit'})
+        break;
+    case 'darwin':
+        console.log('Building macOS packages...')
+        execSync('npm run build:mac', {stdio: 'inherit'})
+        break;
+}
+console.timeLog('Build complete! Check the release the directory for the packages.')
 
